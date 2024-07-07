325f8d2981dd9bf380550c929bc3687bb53ac563
vishrudh raj <vishrudh.shrinivas@gmail.com>
2024-07-07 23:23:55 +0530

    updated vercel

--- index.ts
+++ index.ts
@@ -29,7 +29,7 @@ app.use(
   })
 );
 
-const port = process.env.PORT || 4000;
+const port = 8080 || process.env.PORT || 8080;
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/api/users", userRouter);
 app.use("/api/parties", partyRouter);
