8da09c04cf990b9ec236f20af14056c18f0c854c
vishrudh raj <vishrudh.shrinivas@gmail.com>
2024-07-08 04:51:17 +0530

    cors update

--- index.ts
+++ index.ts
@@ -22,22 +22,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [
-      'http://localhost:5173',
-      'https://ospbl-frontend-test.vercel.app',
-      process.env.FRONTEND_URL as string,
-    ],
-    credentials: true,
-  })
-);
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(204); // No Content
-});
+const corsOptions = {
+  origin: '*', // Adjust as needed
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 3000;
 app.use('/public', express.static(path.join(__dirname, 'public')));
