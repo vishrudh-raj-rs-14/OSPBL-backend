2e52c6ca1ca0caf630680381099b0fcdf80a1dcb
vishrudh raj <vishrudh.shrinivas@gmail.com>
2024-07-08 09:52:48 +0530

    form issues

--- dist/routes/gradeCheckRoutes.js
+++ dist/routes/gradeCheckRoutes.js
@@ -9,4 +9,5 @@ const gradeCheckController_1 = require("../controllers/gradeCheckController");
 const router = express_1.default.Router();
 router.get("/", userController_1.protect, (0, userController_1.restricTo)("ADMIN", "GRADE_CHECKER"), gradeCheckController_1.getGradeCheckData);
 router.post("/", userController_1.protect, (0, userController_1.restricTo)("ADMIN", "GRADE_CHECKER"), gradeCheckController_1.uploadPDF, gradeCheckController_1.processPDF, gradeCheckController_1.addGradeCheckData);
+router.post("/report", userController_1.protect, (0, userController_1.restricTo)("ADMIN", "GRADE_CHECKER"), gradeCheckController_1.uploadfile);
 exports.default = router;
--- routes/gradeCheckRoutes.ts
+++ routes/gradeCheckRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { protect, restricTo } from "../controllers/userController";
-import { addGradeCheckData, getGradeCheckData, processPDF, uploadPDF } from "../controllers/gradeCheckController";
+import { addGradeCheckData, getGradeCheckData, processPDF, uploadPDF, uploadfile } from "../controllers/gradeCheckController";
 
 const router = express.Router();
 
 router.get("/", protect,  restricTo("ADMIN", "GRADE_CHECKER"), getGradeCheckData);
 router.post("/", protect,  restricTo("ADMIN", "GRADE_CHECKER"), uploadPDF, processPDF, addGradeCheckData);
+router.post("/report", protect,  restricTo("ADMIN", "GRADE_CHECKER"), uploadfile);
 
 export default router;
